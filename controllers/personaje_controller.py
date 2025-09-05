03ba29fd684bd93f12b3eb5026b962f4efb63484
Felipe <luis.duranseguel03@gmail.com>
2025-09-04 20:46:50 -0400

    Retorna 2 cartas

--- controllers/personaje_controller.py
+++ controllers/personaje_controller.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from models.personaje import Personaje
 from services.personaje_service import PersonajeService
+from typing import List
+
 
 router = APIRouter(
     prefix="/api-card-demonic/demonic/card",
     tags=["Personajes"]
 )
 
-@router.get("/random", response_model=Personaje)
-def get_random_personaje():
-    return PersonajeService.get_random()
+@router.get("/random", response_model=List[Personaje])
+def get_random_personajes():
+    return PersonajeService.get_random_two()
--- services/personaje_service.py
+++ services/personaje_service.py
@@ -5,9 +5,19 @@ datos = [
     Personaje(nombre="Demonikachu", rango="Demoniaco", fuerza=80, imagen="/static/Demonikachu.png"),
     Personaje(nombre="Peppadiablo", rango="Demonio",   fuerza=50, imagen="/static/Peppadiablo.png"),
     Personaje(nombre="Demonikitty", rango="Demonito",  fuerza=120, imagen="/static/Demonikitty.png"),
+    Personaje(nombre="DoomBall", rango="Demonio",   fuerza=50, imagen="/static/DoomBall.png"),
+    Personaje(nombre="Kael’thar", rango="Demonito",  fuerza=120, imagen="/static/Kaelthar.png"),
+    Personaje(nombre="HellTubbie", rango="Demonito",  fuerza=120, imagen="/static/HellTubbie.png"),
+    Personaje(nombre="Teemonio", rango="Demoniaco",  fuerza=120, imagen="/static/Teemonio.png"),
+    Personaje(nombre="Infergar", rango="Demoniaco",  fuerza=120, imagen="/static/Infergar.png"),
+    Personaje(nombre="Demonipreer", rango="Demonio",  fuerza=120, imagen="/static/Demonipreer.png"),
 ]
 
 class PersonajeService:
     @staticmethod  # Usando el servicio directamente, sin crear objetos
     def get_random() -> Personaje:
         return random.choice(datos)
+
+    @staticmethod
+    def get_random_two() -> list[Personaje]:
+        return random.sample(datos, 2)  # ← Devuelve 2 distintos
